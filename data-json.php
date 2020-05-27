a177414eb90b03deb14d707c89cf162e564f7d48
epalomares <edgaropalomaresm@gmail.com>
2020-05-27 12:00:00 -0400

    agregando json

---
+++ data-json.php
@@ -0,0 +1,12 @@
+<?php
+//ir a labd
+
+//$_GET['fecha'];
+//buscamos en la d
+//$sql="select * from where fecha=".$_GET['fecha'];
+
+$data='[{"objeto": "prueba1","start": 1000,"end": "1200","startLabel": "","endLabel": "","tipoAplicacion": "App","timeline": "App"},{"objeto": "prueba2","start": 2000,"end": "1300","startLabel": "","endLabel": "","tipoAplicacion": "App","timeline": "App"}]';
+
+echo $data;
+
+?>
