72df12d709956c8dd1e39fbf86cd7f8187e8d8f9
epalomares <edgaropalomaresm@gmail.com>
2020-05-27 12:18:55 -0400

    agregando un ejemplo mas

--- data-json.php
+++ data-json.php
@@ -6,7 +6,8 @@
 //$sql="select * from where fecha=".$_GET['fecha'];
 
 $data='[{"objeto": "prueba1","start": 1000,"end": "1200","startLabel": "","endLabel": "","tipoAplicacion": "App","timeline": "App"},
-		{"objeto": "prueba2","start": 1100,"end": "1300","startLabel": "","endLabel": "","tipoAplicacion": "App","timeline": "App"}]';
+		{"objeto": "prueba2","start": 1100,"end": "1300","startLabel": "","endLabel": "","tipoAplicacion": "App","timeline": "App"},
+		{"objeto": "prueba3","start": 1200,"end": "1600","startLabel": "","endLabel": "","tipoAplicacion": "PC","timeline": "PC"}]';
 
 echo $data;
 
